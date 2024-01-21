7c8e310328c5089193c5777c7ceded9b4f31579f
AdielSM <129189553+AdielSM@users.noreply.github.com>
2024-01-20 23:56:04 -0300

    nova classe para carregar contas

---
+++ src/ContaBanco/CarregarContas.java
@@ -0,0 +1,16 @@
+package ContaBanco;
+
+import ContaBanco.exceptions.*;
+
+public class CarregarContas {
+    public static void carregarContas(Banco banco) throws ValorInvalido, NomeInvalido, EmailInvalido, TelefoneInvalido, SenhaInvalida {
+        banco.criarConta("João","João@email.com", "83912345678", "a.aa@aoi1", 1000);
+        banco.criarConta("Maria", "Maria@email.com", "83912345678", "a.aa@aoi2", 2000);
+        banco.criarConta("José", "José@email.com", "83912345678", "a.aa@aoi3", 3000);
+}
+}
+
+
+
+
+
